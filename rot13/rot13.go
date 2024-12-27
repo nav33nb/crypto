303da28e5a63c1e8b6194d9b7ea469419b2bf4e1
nav33nb <Naveen>
2024-12-27 10:23:12 +0000

    crypto/rot13: add docs

--- rot13/rot13.go
+++ rot13/rot13.go
@@ -1,5 +1,8 @@
+// A simple letter substitution cipher, commonly called ROT13
+// It works by shifting any of the 26 alphabets by 13 characters, such that ABC -> NOP and vice versa. Same algorithm provides encoding and decoding
 package rot13
 
+// Accepts an input string to encode/decode, Returns processed string
 func Rot13(input string) string {
 	var floor rune
 	var processed string
