d8938267fe630585da49d67faa717764840a985e
nav33nb <Naveen>
2024-12-27 09:45:58 +0000

    crypto/rot13: update usage

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	rot13 "github.com/nav33nb/crypto/rot13"
+	"github.com/nav33nb/crypto/rot13"
 )
 
 func main() {
-	enc := rot13.Encrypt("Hello World !!!")
-	fmt.Println(enc)
+	str := "Hello"
+	enc := rot13.Encrypt(str)
 	dec := rot13.Decrypt(enc)
-	fmt.Println(dec)
+	fmt.Println("str == dec ?", str == dec)
 }
